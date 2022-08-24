28bf926300f9aea11708f0c69a430a4d71a8befe
Anas Hafsi <anashafsii@gmail.com>
2022-08-24 12:58:30 +0200

    Adding Banner missing files

---
+++ src/components/banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="lines">
+                <div className="description">
+                    <div className="title">
+                        Handmade Silk and Satin Hairwear
+                    </div>
+                    <div className="subtitle">
+                        Handmade protective silk and satin products for
+                        healthier hair and better skin.
+                    </div>
+                </div>
+                <div className="shop-btn">
+                    <a href="/">Shop now</a>
+                </div>
+            </div>
+            <div className="girl"></div>
+        </div>
+    );
+}
+
+export default Banner;
